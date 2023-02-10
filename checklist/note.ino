b1be0f71782a57107d4b90b584ac2ea9c82a59d9
Theresa <49374955+teiResa@users.noreply.github.com>
2023-02-10 22:18:52 +0800

    Create note.ino

---
+++ checklist/note.ino
@@ -0,0 +1,10 @@
+/*
+*Working through Arduino for Dummies
+
+Mostly in review, but I also want to see if it makes more sense now that I am a bit more experienced.
+this is cool. my keyboard slowly turns white the longer you text then when you stop...very purple.
+*
+*/
+
+// blink
+//
